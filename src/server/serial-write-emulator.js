74beb50ec5247c95c9a12320a4e9a6e5c0ac4a80
Qi Mu <skycloud112@gmail.com>
2016-10-18 18:24:21 -0400

    test

--- src/server/serial-write-emulator.js
+++ src/server/serial-write-emulator.js
@@ -9,10 +9,7 @@ port.on('open', function() {
   console.log('Opened port. Going to send example data.');
 
   setInterval(function() {
-    //var integer = chance.integer({min:0, max: 300});
-
     var string = "☻1(  12360    00"
-
     port.write(string);
     console.log('wrote', string);
   }, 1000)
